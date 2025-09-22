eb0f3d62652cc98c1a91704f7e7ea855327bbe22
Lolisuki18 <leninh2004@gmail.com>
2025-09-22 15:40:43 +0700

    Book Controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :update, :destroy]
+
+  def index
+    @books = Book.all
+     render json: @books.to_json(
+  include: [:publisher, :authors , :categories]
+     )
+  end
+
+  def show
+  # render json: @book
+    render json: @book.to_json(
+    include: [:publisher, :authors, :categories]
+  #nó sé trả luôn thông tin của các bảng liên quan (publisher, authors, categories)
+  ) 
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      render json: @book, status: :created
+    else
+      render json: @book.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @book.update!(book_params)
+      render json: @book , status: :ok
+    else
+      render json: @book.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @book.update!(active: false)
+      render json: { message: 'Book delete successfully' } , status: :ok
+    else
+      render json: @book.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def set_book
+    @book = Book.includes(:publisher, :authors, :categories).find(params[:id])
+  end
+  def book_params
+    params.require(:book).permit(:page_count,:title, :publisher_id, :isbn, :publication_date, :price, :stock_quantity, :description, :active, :language , :cover_image_url)
+  end
+
+end
