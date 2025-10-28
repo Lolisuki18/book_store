32f230d0d396267ad6135e1d8181baafffbcb2f4
Lolisuki18 <leninh2004@gmail.com>
2025-10-28 18:29:18 +0700

    update user model add safe_json

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   
   # Validations
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
   validates :user_name, presence: true, uniqueness: true
   validates :full_name, presence: true
   validates :phone, presence: true, uniqueness: true
@@ -13,4 +13,16 @@ class User < ApplicationRecord
   # Enums
   enum :role, { user: 'user', admin: 'admin', staff: 'staff', manager: 'manager' }, default: :user
   enum :gender, { male: "Male", female: "Female", other: "Other" }
+
+def safe_json
+    as_json(except: [:password_digest, :created_at, :updated_at])
+  end
+
+  private
+  # dùng cho cập nhập sẽ ko cần mật khẩu
+  def password_required?
+    new_record? || !password.nil?
+  end
+
+ 
 end
